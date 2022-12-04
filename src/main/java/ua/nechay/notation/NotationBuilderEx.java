75cd3dd2365c773b3c004650239030ba64e3ea25
Aleksei Nechaev <anechaev@devexperts.com>
2022-12-05 02:31:52 +0400

    the feature of determining of language has been added

--- src/main/java/ua/nechay/notation/NotationBuilderEx.java
+++ src/main/java/ua/nechay/notation/NotationBuilderEx.java
@@ -3,12 +3,19 @@ package ua.nechay.notation;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingBuilderEx;
 import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.editor.Document;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author anechaev
@@ -17,11 +24,21 @@ import org.jetbrains.annotations.Nullable;
 public class NotationBuilderEx extends FoldingBuilderEx {
     @Override
     public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
-        if (NotationUtils.hasErrors(root)) {
+        if (!root.getLanguage().is(JavaLanguage.INSTANCE)) {
             return new FoldingDescriptor[0];
         }
-        PsiClass psiClass = PsiTreeUtil.findChildOfType(root, PsiClass.class);
+        if (!PsiTreeUtil.hasErrorElements(root)) {
+            return new FoldingDescriptor[0];
+        }
+        findChildren(root, PsiClass.class)
+            .flatMap(psiClass -> findChildren(psiClass, PsiMethod.class))
+            .flatMap(psiMethod -> findChildren(psiMethod, PsiCodeBlock.class))
+            .collect(Collectors.toList());
         return new FoldingDescriptor[0];
+    }
+
+    private <T extends PsiElement> Stream<T> findChildren(@NotNull PsiElement element, @NotNull Class<T> type) {
+        return PsiTreeUtil.findChildrenOfType(element, type).stream();
     }
 
     @Override
