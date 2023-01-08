46c2b3514939497c8e570f37d10c70d74c469fd9
Aleksei Nechaev <anechaev@devexperts.com>
2023-01-08 16:22:17 +0400

    readme has been added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import org.jetbrains.changelog.markdownToHTML
+
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     id("java")
     id("idea")
     id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
-group = "ua.nechay"
-version = "0.1"
+group = properties("pluginGroup")
+version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
@@ -21,6 +26,11 @@ intellij {
     ))
 }
 
+changelog {
+    groups.set(emptyList())
+    repositoryUrl.set(properties("pluginRepositoryUrl"))
+}
+
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -29,8 +39,21 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("223")
-        untilBuild.set("223.*")
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+        pluginDescription.set(
+            file("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").let { markdownToHTML(it) }
+        )
+
     }
 
     signPlugin {
@@ -40,6 +63,7 @@ tasks {
     }
 
     publishPlugin {
+        dependsOn("patchChangelog")
         token.set(System.getenv("NOTATION_PUBLISH_TOKEN"))
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "notation"
+rootProject.name = "Notation"
